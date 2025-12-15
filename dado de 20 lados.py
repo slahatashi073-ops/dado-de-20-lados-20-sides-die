192baa9afe465866e58a04e6760e79feab82d2d6
slahatashi073-ops <slahatashi073@gmail.com>
2025-12-15 14:48:20 -0300

    Add files via upload

---
+++ dado de 20 lados.py
@@ -0,0 +1,5 @@
+import random
+
+números_dado = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+resultado = random.choice(números_dado)
+print(resultado)
